bd697b35ecd96404f62436f1fbfa82c96258d62b
kuba-bogacki <jb.bogacki@gmail.com>
2025-07-17 19:51:31 +0200

    added global exception handlers for event and notification services & modified tests

---
+++ notification-service/src/main/java/com/notification/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.notification.exception;
+
+public class GlobalExceptionHandler {
+}
---
+++ notification-service/src/main/java/com/notification/exception/SendEmailException.java
@@ -0,0 +1,7 @@
+package com.notification.exception;
+
+public class SendEmailException extends RuntimeException {
+    public SendEmailException(String message) {
+        super(message);
+    }
+}
