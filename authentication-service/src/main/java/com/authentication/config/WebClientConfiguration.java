4e630bf57b3e7915c9d8bce5f8a9ae033acc3556
kuba-bogacki <jb.bogacki@gmail.com>
2026-02-26 22:37:28 +0100

    Fix: remove wrong annotation for prod profile

--- authentication-service/src/main/java/com/authentication/config/WebClientConfiguration.java
+++ authentication-service/src/main/java/com/authentication/config/WebClientConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfiguration {
 
     @Bean
-    @LoadBalanced
     @Profile("production")
     public WebClient.Builder loadBalancedWebClientBuilderProduction() {
         return WebClient.builder();
